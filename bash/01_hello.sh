08f4dcb76c9b48035cc40516fddc96854b393cfe
metalx1000 <github@filmsbykris.com>
2015-02-12 14:43:40 -0500

    basic shell scripts added

---
+++ bash/01_hello.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This is a Comment
+echo "What is your name?"
+read name
+
+echo "What is your sibling's name?"
+read sibling
+
+clear #clears the screen
+
+echo "Hello $name."
+echo "I hope $sibling is doing well."
---
+++ bash/02_sleep_date.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear #clears the screen
+
+echo "What is your name?"
+read name
+
+clear
+echo "Hello $name."
+
+sleep 3 #wait 3 seconds
+echo "The Current Date and Time is:"
+date
+
+sleep 3
+echo "Goodbye $name!"
+sleep 1
